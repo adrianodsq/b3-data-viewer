471e73d5f622f303d68998b62608138a84d40521
Adriano Quast <aquast@uolinc.com>
2022-01-09 21:00:13 -0300

    Front-End Server

---
+++ front-end/public/js/service.js
@@ -0,0 +1,67 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('AppService', AppService);
+
+    // Injects
+    AppService.$inject = ['$http']
+
+    function AppService($http) {
+        // Url computed from the current page address
+        // var urlUtilTlv = '../../admin/service/utilTLV';
+
+        var url = "http://localhost:8785"
+
+
+        // Public API goes here
+        return {
+            buscaDados : _buscaDados,
+            findAllPortfolios : _findAllPortfolios,
+            updatePortfolio : _updatePortfolio
+        }
+
+
+        function _buscaDados(someTicker, successFunc, failureFunc){
+            var endpoint = url + '/ticker/' + someTicker
+            console.log("M=_buscaDados step=start endpoint=" + endpoint)
+            $http.get(endpoint).then(successFunc, failureFunc);
+            console.log("M=_buscaDados step=end endpoint=" + endpoint)
+        }
+
+        function _findAllPortfolios(successFunc, failureFunc){
+            var endpoint = url + '/portfolio/';
+            console.log("M=_findAllPortfolios step=start endpoint=" + endpoint)
+            $http.get(endpoint).then(successFunc, failureFunc);
+            console.log("M=_findAllPortfolios step=end endpoint=" + endpoint)
+        }
+
+        function _updatePortfolio(portfolio, tickerList, successFunc, failureFunc){
+            console.log("M=_updatePortfolio step=start portfolio=" + portfolio + " tickers="+tickerList)
+            var endpoint = url + '/portfolio/' + portfolio;
+            var data = {
+                "tickerList" : tickerList
+            }
+            $http.post(endpoint, data)
+                .then(successFunc)
+                .catch(failureFunc);
+
+        }
+
+        function _post(url, data, successFunc, failureFunc) {
+            var config = {
+                "X-Requested-With": "XMLHttpRequest",
+                "headers" :
+                    {
+                        "mobileUrlTarget" : mobileUrlTarget
+                    },
+            };
+            $http.post(url, data, config)
+                .then(successFunc)
+                .catch(failureFunc);
+        }
+
+    }
+
+})();
---
+++ front-end/server.js
@@ -0,0 +1,97 @@
+const SERVER_PORT = 8785
+const B3_APP_URL = "http://localhost:8080"
+// imports
+const express = require('express');
+const path = require('path');
+// Pra fazer requests
+const axios = require('axios');
+
+// inits
+var app = express();
+app.use(express.json());
+// Arquivos estaticos serao procurados na pasta public
+app.use(express.static('public'));
+
+
+// curl http://localhost:8785/ticker/cyre3
+app.get('/ticker/:someTicker', function(req, res) {
+
+    var ticker = req.params.someTicker;
+    console.log("M=getTicker step=start param=" + ticker);
+    // Exemplo - http://localhost:8080/acoes/cyre3
+    var url = B3_APP_URL + "/b3/acoes/" + ticker.toUpperCase();
+    
+    axios.get(url)
+    .then(resp => {
+        const result = resp.data;
+        console.log("M=getTicker step=end param=" + ticker);
+        //console.log(result);
+        res.send(result);
+    })
+    .catch(err => {
+        console.log("M=getTicker step=error param=" + ticker + " error=" + err);
+        // devolver um objeto mais elegante reportando o erro
+        throw err;
+    });
+
+    
+});
+
+// curl http://localhost:8080/portolio/
+app.get('/portfolio/', function(req, res) {
+    console.log("M=getPortfolios step=start");
+
+    var url = B3_APP_URL + "/portolio/";
+    
+    axios.get(url)
+    .then(resp => {
+        const result = resp.data;
+        console.log("M=getPortfolios step=end");
+        //console.log(result);
+        res.send(result);
+    })
+    .catch(err => {
+        console.log("M=getPortfolios step=error error=" + err);
+        // devolver um objeto mais elegante reportando o erro
+        throw err;
+    });
+    
+});
+
+
+app.post('/portfolio/:somePortfolio/', function(req, res) {
+    var portfolio = req.params.somePortfolio;
+    
+    var tickerList = req.body.tickerList;
+    console.log("M=updatePortfolio port=" + portfolio + " tickers=" + tickerList);
+
+    var url = B3_APP_URL + "/portolio/edit/" + portfolio + "?tickers=" + tickerList
+    console.log("url=" + url)
+    axios.put(url, null)
+    .then(resp => {
+        console.log("M=updatePortfolio step=OK");
+        res.send("OK")
+    })
+    .catch(err => {
+        console.log("M=updatePortfolio step=error error=" + err);
+        // devolver um objeto mais elegante reportando o erro
+        throw err;
+    });
+
+   
+});
+
+
+
+// viewed at http://localhost:8785
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/home.html'));
+});
+
+
+
+app.listen(SERVER_PORT, ()=> {
+    console.log("Server Started! Listening at: " + SERVER_PORT );
+});
+
+
